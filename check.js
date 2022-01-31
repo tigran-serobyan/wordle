1b3edc8d76cfc36bf25764b38883d7df5e202e48
tigran-serobyan <tigran.serobyan.y@tumo.org>
2022-01-31 12:08:04 +0400

    console.log

---
+++ check.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var words = [];
+var repeatingWords = [];
+var nonRepeatingWords = [];
+
+fs.readFile('./repeatingWords.txt', function (err, data) {
+  let words_ = data.toString('utf-8').split('\n');
+  for (let w of words_) {
+    repeatingWords.push(w);
+  }
+  repeatingWords.sort()
+  fs.readFile('./nonRepeatingWords.txt', function (err, data) {
+    let words_ = data.toString('utf-8').split('\n');
+    for (let w of words_) {
+      if (nonRepeatingWords.indexOf(w) == -1) {
+        nonRepeatingWords.push(w);
+      }
+    }
+    nonRepeatingWords.sort();
+    fs.readFile('./words.txt', function (err, data) {
+      let words_ = data.toString('utf-8').split('\n');
+      for (let w of words_) {
+        if (words.indexOf(w) == -1) {
+          words.push(w);
+        }
+      };
+      words.sort();
+      wordOfTheDay = words[Math.ceil(Math.random() * (words.length - 1))].toUpperCase();
+      fs.writeFile('./words.txt', words.join('\n'), function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          fs.writeFile('./nonRepeatingWords.txt', nonRepeatingWords.join('\n'), function (err) {
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+      });
+    });
+  });
+});
+
+router.get('/', function (req, res, next) {
+  if (words.indexOf(req.query.word.toLowerCase()) == -1) {
+    res.send(false);
+    if (repeatingWords.indexOf(req.query.word.toLowerCase()) == -1) {
+      repeatingWords.push(req.query.word.toLowerCase());
+      fs.writeFile('./repeatingWords.txt', repeatingWords.join('\r\n'), function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  } else {
+    res.send(true);
+    if (nonRepeatingWords.indexOf(req.query.word.toLowerCase()) != -1) {
+      nonRepeatingWords.splice(nonRepeatingWords.indexOf(req.query.word.toLowerCase()), 1);
+      fs.writeFile('./nonRepeatingWords.txt', nonRepeatingWords.join('\r\n'), function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  }
+});
