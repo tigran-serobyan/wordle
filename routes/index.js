db6fb1f5393aee5e56264831898694f4a61ebf48
tigran-serobyan <tigran.serobyan.y@tumo.org>
2022-02-23 08:17:58 +0400

    JSON changes

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ var wordNumber = Math.floor((date - new Date("Mon Feb 21 2022 00:00:00 GMT+0400"
 fs.readFile('./stats.txt', function (err, data) {
   stats = JSON.parse(data.toString('utf-8'));
   fs.readFile('./swords.txt', function (err, data) {
-    let swords_ = data.toString('utf-8').split('\n');
+    let swords_ = JSON.parse(data.toString('utf-8'));
     for (let w of swords_) {
       if (swords.indexOf(w) == -1) {
         swords.push(w);
@@ -22,18 +22,18 @@ fs.readFile('./stats.txt', function (err, data) {
     }
     wordOfTheDay = swords[wordNumber].toUpperCase();
     wordNumber += 1;
-    fs.writeFile('./swords.txt', swords.join('\n'), function (err) {
+    fs.writeFile('./swords.txt', JSON.stringify(swords), function (err) {
       if (err) {
         console.log(err);
       } else {
         fs.readFile('./nonRepeatingWords.txt', function (err, data) {
-          let words_ = data.toString('utf-8').split('\n');
+          let words_ = JSON.parse(data.toString('utf-8'));
           for (let w of words_) {
             nonRepeatingWords.push(w);
           }
           nonRepeatingWords.sort()
           fs.readFile('./words.txt', function (err, data) {
-            let words_ = data.toString('utf-8').split('\n');
+            let words_ = JSON.parse(data.toString('utf-8'));
             for (let w of words_) {
               if (words.indexOf(w) == -1) {
                 words.push(w);
@@ -41,22 +41,22 @@ fs.readFile('./stats.txt', function (err, data) {
             }
             words.sort();
             fs.readFile('./repeatingWords.txt', function (err, data) {
-              let words_ = data.toString('utf-8').split('\n');
+              let words_ = JSON.parse(data.toString('utf-8'));
               for (let w of words_) {
                 if (words.indexOf(w) == -1) {
                   repeatingWords.push(w);
                 }
               }
               repeatingWords.sort();
-              fs.writeFile('./repeatingWords.txt', repeatingWords.join('\n'), function (err) {
+              fs.writeFile('./repeatingWords.txt', JSON.stringify(repeatingWords), function (err) {
                 if (err) {
                   console.log(err);
                 } else {
-                  fs.writeFile('./words.txt', words.join('\n'), function (err) {
+                  fs.writeFile('./words.txt', JSON.stringify(words), function (err) {
                     if (err) {
                       console.log(err);
                     } else {
-                      fs.writeFile('./nonRepeatingWords.txt', nonRepeatingWords.join('\n'), function (err) {
+                      fs.writeFile('./nonRepeatingWords.txt', JSON.stringify(nonRepeatingWords), function (err) {
                         if (err) {
                           console.log(err);
                         }
@@ -95,7 +95,7 @@ router.get('/checkWord/:word', function (req, res, next) {
     res.send(false);
     if (repeatingWords.indexOf(req.params.word.toLowerCase()) == -1) {
       repeatingWords.push(req.params.word.toLowerCase());
-      fs.writeFile('./repeatingWords.txt', repeatingWords.join('\n'), function (err) {
+      fs.writeFile('./repeatingWords.txt', JSON.stringify(repeatingWords), function (err) {
         if (err) {
           console.log(err);
         }
@@ -105,7 +105,7 @@ router.get('/checkWord/:word', function (req, res, next) {
     res.send(true);
     if (nonRepeatingWords.indexOf(req.params.word.toLowerCase()) != -1) {
       nonRepeatingWords.splice(nonRepeatingWords.indexOf(req.params.word.toLowerCase()), 1);
-      fs.writeFile('./nonRepeatingWords.txt', nonRepeatingWords.join('\n'), function (err) {
+      fs.writeFile('./nonRepeatingWords.txt', JSON.stringify(nonRepeatingWords), function (err) {
         if (err) {
           console.log(err);
         }
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,11 @@ var fs = require('fs');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   fs.readFile('./repeatingWords.txt', function (err, data) {
-    let repeatingWords = data.toString('utf-8');
+    let repeatingWords = JSON.parse(data.toString('utf-8')).join('\n');
     fs.readFile('./nonRepeatingWords.txt', function (err, data) {
-      let nonRepeatingWords = data.toString('utf-8');
+      let nonRepeatingWords = JSON.parse(data.toString('utf-8')).join('\n');
       fs.readFile('./words.txt', function (err, data) {
-        let words = data.toString('utf-8');
+        let words = JSON.parse(data.toString('utf-8')).join('\n');
         res.render('admin', { title: 'Բառուկ', words, nonRepeatingWords, repeatingWords });
       });
     });
