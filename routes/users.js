46fdcd42f91ac5bf92f45e4dd21330811c52fee5
tigran-serobyan <tigran.serobyan.y@tumo.org>
2022-02-24 21:52:58 +0400

    :)

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,9 @@ router.get('/', function(req, res, next) {
   fs.readFile('./repeatingWords.txt', function (err, data) {
     let repeatingWords = JSON.parse(data.toString('utf-8')).join('\n');
     fs.readFile('./nonRepeatingWords.txt', function (err, data) {
-      let nonRepeatingWords = JSON.parse(data.toString('utf-8')).join('\n');
+      let nonRepeatingWords = data.toString('utf-8');
       fs.readFile('./words.txt', function (err, data) {
-        let words = JSON.parse(data.toString('utf-8')).join('\n');
+        let words = data.toString('utf-8');
         res.render('admin', { title: 'Բառուկ', words, nonRepeatingWords, repeatingWords });
       });
     });
